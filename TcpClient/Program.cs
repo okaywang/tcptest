49c89896dadc93affd3d29f91d3cee3d488993d8
okaywang <44@qq.com>
2016-02-17 16:04:17 +0800

    edit

---
+++ TcpClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var client = new System.Net.Sockets.TcpClient();
+            client.Connect("192.168.7.227", 9731);
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (!string.IsNullOrEmpty(line))
+                {
+                    Send(client, line);
+                }
+            }
+        }
+
+        static void Send(System.Net.Sockets.TcpClient client, string text)
+        {
+            var stream = client.GetStream();
+            var bytes = System.Text.Encoding.Default.GetBytes(text);
+            stream.Write(bytes, 0, bytes.Length);
+
+
+            var buffer = new byte[256];
+            var count = stream.Read(buffer, 0, buffer.Length);
+            var data = new byte[count];
+            Array.Copy(buffer, data, data.Length);
+            var result = System.Text.Encoding.Default.GetString(data);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TcpServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Listen(IPAddress.Any, 9731);
+        }
+
+        static void Listen(IPAddress ip, int port)
+        {
+            var listener = new TcpListener(new IPEndPoint(ip, port));
+            listener.Start();
+
+            using (var client = listener.AcceptTcpClient())
+            {
+                using (var stream = client.GetStream())
+                {
+                    while (true)
+                    {
+                        var buffer = new byte[256];
+                        var count = stream.Read(buffer, 0, buffer.Length);
+
+                        var data = new byte[count];
+                        Array.Copy(buffer, data, data.Length);
+                        var text = System.Text.Encoding.Default.GetString(data);
+                        var result = text.ToUpper();
+
+                        var resultBytes = System.Text.Encoding.Default.GetBytes(result);
+                        stream.Write(resultBytes, 0, resultBytes.Length);
+                    }
+                }
+            }
+        }
+    }
+}
